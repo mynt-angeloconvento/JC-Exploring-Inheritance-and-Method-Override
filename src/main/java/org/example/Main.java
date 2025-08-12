daa149047755c895c1434d42cc1f4b766b49590a
Godfrey Angelo Convento <godfrey-a.convento@gcash.com>
2025-08-12 17:28:51 +0800

    feat: complete mini-challenge 2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,11 +5,18 @@ public class Main {
         // Car myCar = new Car();
         // myCar.honk();
         // myCar.display();
-        Bicycle bicycle = new Bicycle();
-        System.out.print("Bicycle says: ");
-        bicycle.ringBell();
-        System.out.print("Bicycle says: ");
-        bicycle.honk();
-        
+        // Bicycle bicycle = new Bicycle();
+        // System.out.print("Bicycle says: ");
+        // bicycle.ringBell();
+        // System.out.print("Bicycle says: ");
+        // bicycle.honk();
+
+        // Circle myCircle = new Circle();
+        // System.out.println(myCircle.display());
+        // Rectangle myRectangle = new Rectangle();
+        // System.out.println(myRectangle.display());
+        Dog myDog = new Dog();
+        myDog.breathe();
+        myDog.makeSound();
     }
 }
